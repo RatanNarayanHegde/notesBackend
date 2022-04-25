212dbd9df1417df589e9c5e31545133f73a6a66e
RatanNarayanHegde <ratanhegde08@gmail.com>
2022-04-25 08:08:09 +0530

    use express and add routes

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const http = require("http");
+const express = require("express");
+const { json } = require("express/lib/response");
+const app = express();
 
 let notes = [
   {
@@ -21,12 +23,33 @@ let notes = [
   },
 ];
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "application/json" });
-  response.end(JSON.stringify(notes));
+app.get("/", (request, response) => {
+  response.send("<h1> Hello, World! </h1>");
+});
+
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
+
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
 });
 
 const PORT = 3001;
-app.listen(PORT);
-
-console.log(`The server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`The server running on port ${PORT}`);
+});
