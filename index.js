d53196313736544f86c45d39750cd808080956cb
RatanNarayanHegde <ratanhegde08@gmail.com>
2022-04-30 08:39:44 +0530

    add middleware

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express = require("express");
 const { json } = require("express/lib/response");
 const app = express();
 
+app.use(express.json());
+
+const requestLogger = (request, response, next) => {
+  console.log(request.method);
+  console.log(request.path);
+  console.log(request.body);
+  console.log("-----");
+  next();
+};
+
+app.use(requestLogger);
+
 let notes = [
   {
     id: 1,
@@ -23,6 +35,11 @@ let notes = [
   },
 ];
 
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
 app.get("/", (request, response) => {
   response.send("<h1> Hello, World! </h1>");
 });
@@ -42,12 +59,38 @@ app.get("/api/notes/:id", (request, response) => {
   }
 });
 
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content is missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
+});
+
 app.delete("/api/notes/:id", (request, response) => {
   const id = Number(request.params.id);
   notes = notes.filter((note) => note.id !== id);
 
   response.status(204).end();
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
