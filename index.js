9e3682be568d290b5b7b46b433903211e820d354
RatanNarayanHegde <ratanhegde08@gmail.com>
2022-05-14 21:28:56 +0530

    add database to save note

--- index.js
+++ index.js
@@ -57,34 +57,27 @@ app.get("/api/notes", (request, response) => {
 });
 
 app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-
-  if (note) {
+  Note.findById(request.params.id).then((note) => {
     response.json(note);
-  } else {
-    response.status(404).end();
-  }
+  });
 });
 
 app.post("/api/notes", (request, response) => {
   const body = request.body;
-  if (!body.content) {
-    return response.status(400).json({
-      error: "content is missing",
-    });
+
+  if (body.content === undefined) {
+    return response.status(400).json({ error: "content missing" });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
-
-  response.json(note);
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
 app.delete("/api/notes/:id", (request, response) => {
